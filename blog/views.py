37385f342b82a92d2dd89e0a8a3bb927ff62ad47
PHENPHICHA <5910110236@psu.ac.th>
2019-02-17 15:54:16 +0000

    Add Image Gallery

--- blog/views.py
+++ blog/views.py
@@ -16,9 +16,11 @@ def post_detail(request, pk):
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
+        
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -27,6 +29,7 @@ def post_new(request):
     
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.published_date = timezone.now()
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
